5680fa819b6dc4497a8968f344c7a7429e719ef9
Moulik <moulik3925@gmail.com>
2024-11-02 23:11:44 +0530

    made part 1 more robust

--- phase1/match_submissions.hpp
+++ phase1/match_submissions.hpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
 // -----------------------------------------------------------------------------
 
 // You are free to add any STL includes above this comment, below the --line--.
@@ -17,10 +18,13 @@ using std::endl;
 // OPTIONAL: Add your helper functions and data structures here
 const int BASE = 31;
 const int MODULUS = 1e9 + 9;
-ll exponent(ll base, ll power){
+ll exponent(ll base, ll power)
+{
     ll result = 1;
-    while(power > 0){
-        if(power % 2 == 1){
+    while (power > 0)
+    {
+        if (power % 2 == 1)
+        {
             result = (result * base) % MODULUS;
         }
         base = (base * base) % MODULUS;
@@ -42,12 +46,23 @@ ll compute_hash(std::vector<int> &submission, int start, int end)
 ll update_hash(ll old_hash, int old_char, int new_char, int length)
 {
     ll new_hash = (old_hash - old_char * exponent(BASE, length - 1)) % MODULUS;
-    if(new_hash < 0)
+    if (new_hash < 0)
     {
         new_hash += MODULUS;
     }
     new_hash = (new_hash * BASE + new_char) % MODULUS;
     return new_hash;
+}
+
+ll sum_of_k_greatest_elements(std::vector<ll> &arr, int k)
+{
+    std::sort(arr.begin(), arr.end(), std::greater<ll>());
+    ll sum = 0;
+    for (int i = 0; i < std::min(k, (int)arr.size()); i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
 }
 
 std::array<int, 5> match_submissions(std::vector<int> &submission1,
@@ -63,7 +78,8 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
     int start_index1 = 0;
     int start_index2 = 0;
     // std::unordered_set<ll> hashes;
-    std::unordered_map<ll, ll> hashes;
+    std::unordered_map<ll, std::vector<ll>> hashes;
+    std::unordered_map<ll, std::vector<ll>> hash_score;
     ll window_length = 13;
     ll hash1 = compute_hash(submission1, 0, window_length);
     ll rolling_hash = hash1;
@@ -72,9 +88,9 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
         // hashes.insert(hash1);
         // hash1 = compute_hash(submission1, i, i + window_length);
         hash1 = rolling_hash;
-        hashes[hash1] = i;
+        hashes[hash1].push_back(i);
         // cout<<hash1<<" "<<rolling_hash<<endl;
-    
+
         if (i + window_length < n)
         {
             rolling_hash = update_hash(hash1, submission1[i], submission1[i + window_length], window_length);
@@ -92,7 +108,7 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
         if (hashes.find(hash2) != hashes.end())
         {
 
-            int match_index = hashes[hash2];
+            std::vector<ll> match_indices = hashes[hash2];
             // int length_matching = 0;
             // while (j + length_matching < m && submission1[start_index1 + length_matching] == submission2[j + length_matching])
             // {
@@ -103,15 +119,23 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
             //     total_matching_length = length_matching;
             //     start_index2 = j;
             // }
-            int length_matching = 0;
-            while (match_index + length_matching < n && j + length_matching < m && submission1[match_index + length_matching] == submission2[j + length_matching])
+            int max_length_matching = 0;
+            for (auto match_index : match_indices)
             {
-                length_matching++;
+                int length_matching = 0;
+                while (match_index + length_matching < n && j + length_matching < m && submission1[match_index + length_matching] == submission2[j + length_matching])
+                {
+                    length_matching++;
+                }
+                max_length_matching = std::max(max_length_matching, length_matching);
             }
             // std::cout << "Match found at " << j << " of length " << length_matching << std::endl;
-            total_matching_length += length_matching;
-            j += length_matching;
-            if(j > m - window_length)
+
+            // hash_score[hash2] = std::max((ll)max_length_matching, hash_score[hash2]);
+            hash_score[hash2].push_back(max_length_matching);
+            // total_matching_length += length_matching;
+            j += max_length_matching;
+            if (j > m - window_length)
             {
                 break;
             }
@@ -125,7 +149,10 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
         // else
         //     break;
     }
-
+    for (auto it : hash_score)
+    {
+        total_matching_length += sum_of_k_greatest_elements(it.second, hashes[it.first].size());
+    }
     // for (int shift = 0; shift < n; shift++)
     // {
     //     for (int match_index = 0; match_index < m; match_index++)
@@ -137,7 +164,7 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
     //         }
     //     }
     // }
-    if(total_matching_length > 0.3 * std::min(n, m))
+    if (total_matching_length > 0.3 * std::min(n, m))
     {
         plagiarism_flag = 1;
     }
