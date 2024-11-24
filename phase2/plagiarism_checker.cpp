44dfb060d0f7aa8e1828fab16d739458962e49de
Mr.OP <prakharjain3103@gmail.com>
2024-11-24 18:54:52 +0530

    first push pj

--- phase2/plagiarism_checker.cpp
+++ phase2/plagiarism_checker.cpp
@@ -4,4 +4,38 @@
 
 // TODO: Implement the methods of the plagiarism_checker_t class
 
+#define ll long long
+using namespace std;
+ll BASE = 37;
+ll MODULUS = 1e9 + 9;
+
+ll compute_hash(std::vector<int> &submission, int start, int end)
+{
+    ll hash = 0;
+    for (int i = start; i < end; i++)
+    {
+        hash = (hash * BASE + submission[i]) % MODULUS;
+    }
+    return hash;
+}
+
+plagiarism_checker_t::plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>>
+                                               __submissions)
+{
+    submissions = __submissions;
+    // for(int i = 0; )
+
+
+
+}
+plagiarism_checker_t::~plagiarism_checker_t()
+{
+
+}
+
+void plagiarism_checker_t::add_submission(std::shared_ptr<submission_t> __submission)
+{
+    
+}
+
 // End TODO
--- phase2/plagiarism_checker.hpp
+++ phase2/plagiarism_checker.hpp
@@ -1,4 +1,11 @@
 #include "structures.hpp"
+#include<chrono>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<utility>
+#include<memory>
+
 // -----------------------------------------------------------------------------
 
 // You are free to add any STL includes above this comment, below the --line--.
@@ -7,17 +14,21 @@
 
 // OPTIONAL: Add your helper functions and classes here
 
-class plagiarism_checker_t {
+class plagiarism_checker_t
+{
     // You should NOT modify the public interface of this class.
 public:
     plagiarism_checker_t(void);
-    plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> 
-                            __submissions);
+    plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>>
+                             __submissions);
     ~plagiarism_checker_t(void);
     void add_submission(std::shared_ptr<submission_t> __submission);
 
 protected:
     // TODO: Add members and function signatures here
     
+    std::unordered_map<long long, std::vector<std::pair<std::shared_ptr<submission_t>, long long>>> hashes;
+    std::vector<std::shared_ptr<submission_t>> submissions;
+    std::map<std::shared_ptr<submission_t>, std::chrono::system_clock::time_point> time_stamps;
     // End TODO
 };
